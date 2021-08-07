69452de6451ad9e6dfcf8e8887170cc04982a236
mendesnathanj <mendesnathanj@gmail.com>
2021-08-07 16:12:24 -0700

    generate db

---
+++ db/migrate/20210807230407_add_tables.rb
@@ -0,0 +1,32 @@
+class AddTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+
+    create_table :lists do |t|
+      t.string :title
+
+      t.timestamps
+    end
+
+    create_join_table :users, :lists do |t|
+      t.index :user_id
+      t.index :list_id
+      t.boolean :read_access, default: true, null: false
+      t.boolean :write_access, default: true, null: false
+      t.boolean :delete_access, default: true, null: false
+    end
+
+    create_table :todos do |t|
+      t.text :description
+      t.boolean :completed, default: false, null: false
+      t.references :list
+
+      t.timestamps
+    end
+  end
+end
